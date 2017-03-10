160c42d5a8ff6c8f88af2fde507b5e7033083102
demvy <demes661@gmail.com>
2017-03-10 21:31:48 +0200

    term use first tutor not work

--- ft_select/includes/ft_select.h
+++ ft_select/includes/ft_select.h
@@ -2,8 +2,10 @@
 #ifndef _FT_SELECT_H_
 # define _FT_SELECT_H_
 
-#include "../libft/libft.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <termios.h>
-#include <term.h>
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <termios.h>
+# include <term.h>
+
+#endif
--- ft_select/src/main.c
+++ ft_select/src/main.c
@@ -1,1 +1,43 @@
 
+#include "ft_select.h"
+
+int		ft_do()
+{
+	char *buffer[3];
+
+	while (1)
+	{
+		read(0, buffer, 3);
+		if (ft_strchr(buffer, 27) != NULL)
+			printf("C'est une fleche !\n");
+    	else if (ft_strchr(buffer, 4) != NULL)
+    	{
+      		printf("Ctlr+d, on quitte !\n");
+      		return (0);
+      	}
+	}
+	return (0);
+}
+
+int		main(int ac, char **av, char **env)
+{
+	char			*name_term;
+	struct termis	term;
+
+	if ((name_term = getenv("TERM")) == NULL)
+		return (-1);
+	if (tgetent(NULL, name_term) == ERR)
+		return (-1);
+	if (tcgetattr(0, term) == -1)
+		return (-1);
+	/*
+	term.c_lflag &= ~(ICANON);
+	term.c_lflag &= ~(ECHO);
+	term.c_cc[VMIN] = 1;
+	term.c_cc[VTIME] = 0;
+	if (tcsetattr(0, TCSADRAIN, &term) == -1)
+		return (-1);
+		*/
+	ft_do();
+	return (0);
+}
