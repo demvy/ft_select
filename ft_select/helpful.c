1ef7f3bee924a1a482427b17db0a21c28b64e689
demvy <demes661@gmail.com>
2017-03-20 10:34:17 +0200

    signals added

---
+++ ft_select/helpful.c
@@ -0,0 +1,52 @@
+
+#include "ft_select.h"
+
+void	change_str(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '\t')
+			s[i] = ' ';
+		i++;
+	}
+}
+
+size_t	ft_substrlen(const char *s, char c)
+{
+	size_t  i;
+
+	i = 0;
+	while (*s != c && *s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+int     put(int c)
+{
+	write(STDOUT_FILENO, &c, 1);
+	return (0);
+}
+
+size_t  max_len(t_lst_d *lst)
+{
+	t_lst_d *first;
+	t_lst_d *curr;
+	size_t  max;
+
+	first = lst;
+	curr = lst;
+	max = 1;
+	while (curr->next != first->next)
+	{
+		if (ft_strlen(curr->data) > max)
+			max = ft_strlen(curr->data);
+		curr = curr->next;
+	}
+	return (max);
+}
---
+++ ft_select/show_func.c
@@ -0,0 +1,21 @@
+//
+// Created by valeriy on 19.03.17.
+//
+
+#include "ft_select.h"
+
+
+void    show_args(t_term *term)
+{
+	t_lst_d *first;
+	t_lst_d *curr;
+	int     cols;
+	int     rows;
+
+	first = term->args;
+	curr = term->args;
+	while (curr->next != first->next)
+	{
+
+	}
+}
